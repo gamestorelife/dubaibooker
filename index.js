529db1fe2f2ea41922f03756572bbc1c0e8f66ea
gamestorelife <freerapper666@gmail.com>
2024-01-11 08:08:02 +0200

    update on post
    [TAILING] Tailing last 15 lines for [all] processes (change the value with --lines option)
    /home/u157487157/.pm2/pm2.log last 15 lines:
    PM2        | 2024-01-11T05:27:16: PM2 log: Stopping app:index id:0
    PM2        | 2024-01-11T05:27:16: PM2 log: App [index:0] exited with code [0] via signal [SIGINT]
    PM2        | 2024-01-11T05:27:16: PM2 log: pid=7923 msg=process killed
    PM2        | 2024-01-11T05:27:16: PM2 log: App [index:0] starting in -fork mode-
    PM2        | 2024-01-11T05:27:16: PM2 log: App [index:0] online
    PM2        | 2024-01-11T05:50:21: PM2 log: Stopping app:index id:0
    PM2        | 2024-01-11T05:50:21: PM2 log: App [index:0] exited with code [0] via signal [SIGINT]
    PM2        | 2024-01-11T05:50:21: PM2 log: pid=16762 msg=process killed
    PM2        | 2024-01-11T05:50:21: PM2 log: App [index:0] starting in -fork mode-
    PM2        | 2024-01-11T05:50:21: PM2 log: App [index:0] online
    PM2        | 2024-01-11T05:54:02: PM2 log: Stopping app:index id:0
    PM2        | 2024-01-11T05:54:02: PM2 log: App [index:0] exited with code [0] via signal [SIGINT]
    PM2        | 2024-01-11T05:54:02: PM2 log: pid=8417 msg=process killed
    PM2        | 2024-01-11T06:02:47: PM2 log: App [index:0] starting in -fork mode-
    PM2        | 2024-01-11T06:02:47: PM2 log: App [index:0] online
    
    /home/u157487157/.pm2/logs/index-error.log last 15 lines:
    0|index    |     at Module._compile (internal/modules/cjs/loader.js:1114:14)
    0|index    |     at Object.Module._extensions..js (internal/modules/cjs/loader.js:1143:10)
    0|index    |     at Module.load (internal/modules/cjs/loader.js:979:32)
    0|index    |     at Function.Module._load (internal/modules/cjs/loader.js:819:12)
    0|index    | ReferenceError: app is not defined
    0|index    |     at Object.<anonymous> (/home/u157487157/domains/dubaibooker.com/public_html/index.js:84:1)
    0|index    |     at Module._compile (internal/modules/cjs/loader.js:1114:14)
    0|index    |     at Object.Module._extensions..js (internal/modules/cjs/loader.js:1143:10)
    0|index    |     at Module.load (internal/modules/cjs/loader.js:979:32)
    0|index    |     at Function.Module._load (internal/modules/cjs/loader.js:819:12)
    0|index    |     at Object.<anonymous> (/home/u157487157/.nvm/versions/node/v14.21.3/lib/node_modules/pm2/lib/ProcessContainerFork.js:33:23)
    0|index    |     at Module._compile (internal/modules/cjs/loader.js:1114:14)
    0|index    |     at Object.Module._extensions..js (internal/modules/cjs/loader.js:1143:10)
    0|index    |     at Module.load (internal/modules/cjs/loader.js:979:32)
    0|index    |     at Function.Module._load (internal/modules/cjs/loader.js:819:12)
    
    /home/u157487157/.pm2/logs/index-out.log last 15 lines:
    0|index    | Server running at http://localhost:3000
    0|index    | Server running at http://localhost:3000
    0|index    | Server running at http://localhost:3000
    0|index    | Server running at http://localhost:3000
    0|index    | Server running at http://localhost:3000
    0|index    | Server running at http://localhost:3000
    0|index    | Server running at http://localhost:3000

--- index.js
+++ index.js
@@ -80,7 +80,8 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
