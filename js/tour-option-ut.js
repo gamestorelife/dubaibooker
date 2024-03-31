9e9f9cf7d2c8653d6620f39bb65f3c27a7878291
gamestorelife <freerapper666@gmail.com>
2024-03-31 07:00:14 +0200

    updated
    updated

--- js/tour-option-ut.js
+++ js/tour-option-ut.js
@@ -179,7 +179,7 @@ document.addEventListener("DOMContentLoaded", async function () {
           <div class="fonticon" id="popup-button">
             <i class="fa-regular fa-arrow-left"></i>
           </div>
-          <div><h2>${tour.tourName}</h2></div>
+          <div><h4>${tour.tourName}</h4></div>
           <div class="fonticon"><i class="fa-regular fa-cart-shopping-fast"></i></div>
         </div>
       
